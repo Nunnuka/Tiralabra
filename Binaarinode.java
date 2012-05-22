9dbc4d0bf9c6510aa94951af52d76910d4cb3bf1
Annika <annika.nykanen@helsinki.fi>
2012-05-22 10:38:16 +0300

    Solmu lisatty

---
+++ Binaarinode.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Annika
+ */
+public class Binaarinode {
+
+    int value;
+    Binaarinode parent;
+    Binaarinode left;
+    Binaarinode right;
+
+    public Binaarinode(int value) {
+        this.value = value;
+    }
+
+    public Binaarinode() {
+    }
+}

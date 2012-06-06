a4baa13643e0903084fd8d04f4baa372b20202df
Nunnuka <annika.nykanen@helsinki.fi>
2012-06-06 13:27:53 +0300

    Fibosolmu lisatty

---
+++ src/main/java/Fibonaccinode.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TiraAnnika;
+
+/**
+ *
+ * @author Annika
+ */
+public class FibonacciNode {
+
+
+    int arvo;
+    int aste;
+    boolean mark;
+    FibonacciNode lapsi;
+    FibonacciNode vasen;
+    FibonacciNode oikea;
+    FibonacciNode vanhempi;
+
+    public FibonacciNode(int arvo){
+        this.arvo = arvo;
+    }
+
+}
